cb6f5756997ec013de7104c1c01c6a3a0b90f364
Arif Iqbal <arif_iqbal@yahoo.com>
2023-01-16 19:09:25 -0600

    Typescript Fundamentals: 1. Setting TS env. 2. TSC compiler.

---
+++ Fundamentals/01_why_typescript.ts
@@ -0,0 +1,9 @@
+const courseName = "Typescript Bootcamp v3";
+
+printCourseName(courseName);
+
+
+function printCourseName(name:string) {
+    console.log(`The course name is ${name}`.toUpperCase())
+
+}
