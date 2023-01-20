29b4a7314ab03f2840c4c0a48a0013aaf367415e
Arif Iqbal <arif_iqbal@yahoo.com>
2023-01-20 16:30:59 -0600

    23_introduction_to_functions 24_function_types

---
+++ Fundamentals/23_introduction_to_functions.js
@@ -0,0 +1,11 @@
+//  Added Interface Type Course in the function declaration
+function createCourse(title, subtitle, lessonsCount) {
+    console.log(" Creating course with Title: ".concat(title, ", \n    Subtitle: ").concat(subtitle, " lessons count: ").concat(lessonsCount));
+    return {
+        title: title,
+        subtitle: subtitle,
+        lessonsCount: lessonsCount
+    };
+}
+var result = createCourse('Typescript Bootcamp', 'Learn the language fundamentals', 100);
+console.log(typeof createCourse);
---
+++ Fundamentals/23_introduction_to_functions.ts
@@ -0,0 +1,28 @@
+interface Course {
+  title: string;
+  subtitle: string;
+  lessonsCount: number;
+}
+//  Added Interface Type Course in the function declaration
+function createCourse(
+  title: string,
+  subtitle: string,
+  lessonsCount: number
+): Course {
+  console.log(` Creating course with Title: ${title}, 
+    Subtitle: ${subtitle} lessons count: ${lessonsCount}`);
+
+  return {
+    title,
+    subtitle,
+    lessonsCount,
+  };
+}
+
+const result = createCourse(
+  'Typescript Bootcamp',
+  'Learn the language fundamentals',
+  100
+);
+
+console.log(typeof createCourse);
