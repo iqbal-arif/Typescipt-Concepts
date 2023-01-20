bef25617aae91feb563396f9a8c94d16715dad0d
Arif Iqbal <arif_iqbal@yahoo.com>
2023-01-19 20:30:10 -0600

    15_default_exports

--- Fundamentals/12.2_modules_imports.ts
+++ Fundamentals/12.2_modules_imports.ts
@@ -7,3 +7,18 @@ const pageSize = PAGE_SIZE;
 // This is how Re-EXPORT works for the third party package
 
 import { CourseType, loadAllCourses, saveCourse } from './14_module_reexports';
+
+import {
+  COURSE_TOTAL,
+  TYPESCRIPT_COURSE,
+  //   printTypescriptCourse,
+} from './15_default_exports';
+
+// To import multiple constants  at once
+import * as constants from './15_default_exports';
+// Now to import specific constant, do the followings
+constants.TYPESCRIPT_COURSE;
+
+// DEFAULT IMPORT
+import moduleName from './15_default_exports';
+moduleName({ constants });
