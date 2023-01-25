aced10e9aa6785b022c8bf468137cb47374e5291
Arif Iqbal <arif_iqbal@yahoo.com>
2023-01-24 21:50:34 -0600

    added 02_partials

---
+++ Generics/02_partials.ts
@@ -0,0 +1,28 @@
+// Custom Course Interface
+export interface Course {
+  /*  In interface type modifying of one or two properties is only possible if "?" is placed next to each value as shown . "?" means the value is OPTIONAL
+    title?:string;
+    subtitle?:string;
+    lessonsCount?: number;
+*/
+  /****PARTIAL WAY**/
+  title: string;
+  subtitle: string;
+  lessonsCount: number;
+}
+
+// Using PARTIAL to update single or multiple mixed properties without any compile error
+export function updateCourse(courseId: string, update: Partial<Course>) {}
+
+updateCourse('1', {
+  title: 'New version of title',
+});
+
+updateCourse('1', {
+  subtitle: 'New version of subtitle',
+});
+
+updateCourse('1', {
+  title: 'New version of title',
+  lessonsCount: 100,
+});
