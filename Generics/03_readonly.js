ed40ed6ae8f0aa3bc0611b2629e0f9c6a0749029
Arif Iqbal <arif_iqbal@yahoo.com>
2023-01-24 21:58:57 -0600

    added 03_readonly js

---
+++ Generics/03_readonly.js
@@ -0,0 +1,9 @@
+function freezeCourse(course) {
+    return Object.freeze(course);
+}
+var frozen = freezeCourse({
+    title: 'Typescript Bootcamp',
+    subtitle: 'Learn the language, build practical projects',
+    lessonsCount: 100
+});
+//frozen.title = "";
