6f2657207b96a15e366f2aed914c13f5212ad49a
Arif Iqbal <arif_iqbal@yahoo.com>
2023-01-25 12:48:29 -0600

    added new 04_generic_functions added new 05_merge_gneric_functions

---
+++ Generics/04_generic_functions.ts
@@ -0,0 +1,59 @@
+// Type Interface are used to define property types and use its interface name with function or objects
+/***COURSE INTERFACE**/
+interface Course {
+  title: string;
+  subtitle: string;
+  lessonsCount: number;
+}
+/**freezeCourse function USES COURSE INTERFACE to return FreezeCourse Object based on Interface */
+/***function parameter course is based on InterFace Course */
+// EXPORT as a prefix is used since two function with same name gives error in TYPESCRIPT. Take out export and see the error
+export function freezeCourse(course: Course): Readonly<Course> {
+  return Object.freeze(course);
+}
+/* Calling freezeCourse with course parameter
+const frozenCourse = freezeCourse(course);
+*/
+/**freezeLesson function USES COURSE INTERFACE to return FreezeCourse Object based on Interface */
+/***function parameter lesson is based on InterFace Lesson given below */
+function freezeLesson(lesson: Lesson): Readonly<Lesson> {
+  return Object.freeze(lesson);
+}
+/**GENERIC Function Definition that accommodates both freezeCourse & freezeLesson function.
+ * Uses Generic function Type, Generic Input, Generic Output, & Extends
+ */
+function freeze<T extends object>(input: T): Readonly<T> {
+  return Object.freeze(input);
+}
+
+// Course OBJECT Property definition
+const course: Course = {
+  title: 'Typescript Bootcamp',
+  subtitle: 'Learn the language, build practical projects',
+  lessonsCount: 100,
+};
+// freezeCourse is now Generic freeze function with course as input
+const frozenCourse = freeze(course);
+
+// const frozenNumber = freeze("10");
+
+//frozenCourse.title = "";
+
+/***LESSON INTERFACE**/
+interface Lesson {
+  title: string;
+  seqNo: number;
+}
+
+// freezeCourse is now Generic freeze function with course as input
+const frozenLesson = freeze({
+  title: 'Lesson Title',
+  seqNo: 10,
+});
+
+/**GENERIC Function Definition that accommodates both freezeCourse & freezeLesson function.
+
+const frozenLesson = freeze({
+  title: 'Lesson Title',
+  seqNo: 10,
+});*/
